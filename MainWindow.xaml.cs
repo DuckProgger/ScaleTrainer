6efac84034b485a4dd163fdde073b3b802dd9915
user <user@user.user>
2021-04-01 00:04:06 +1000

    ++

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Shapes;
-using System.Windows.Media;
-using System;
 
 namespace Scale_Trainer
 {
@@ -17,9 +16,10 @@ namespace Scale_Trainer
         private Guitar guitar;
         private Scale scale;
         private StringedVisualisation guitarVis;
-        int fret = 0;
-        double[] k;
-        event EventHandler ParameterChanged;
+        private int fret = 0;
+        private double[] fretRanges;
+
+        private event EventHandler ParameterChanged;
         private readonly int maxFrets = 24;
 
         public MainWindow()
@@ -29,6 +29,136 @@ namespace Scale_Trainer
             ParameterChanged += TryCreateVisualization;
             Key.Items.Add(Note.NoteName.D);
             Key.Items.Add(Note.NoteName.C);
+        }
+
+        private void TryCreateVisualization(object sender, EventArgs e)
+        {
+            try
+            {
+                CreateVisualization();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CreateVisualization()
+        {
+            if (AllParametersSet())
+            {
+                guitar = new Guitar(selectedStrings.Value, selectedFrets.Value, selectedTuning);
+                scale = new Scale(selectedScale, selectedKey);
+                guitarVis = new StringedVisualisation(guitar, scale);
+                ClearNeck();
+                CreateNeckRows(selectedStrings.Value);
+                CreateNutRows(selectedStrings.Value);
+                CreateFretsOnNeck(selectedStrings.Value, selectedFrets.Value);
+                CreateFretsOnNut(selectedStrings.Value);
+            }
+        }
+
+        private bool AllParametersSet()
+        {
+            return selectedStrings.HasValue && selectedFrets.HasValue && selectedTuning != null && selectedScale != null && selectedKey != 0;
+        }
+
+        private void CreateNeckColumns(int number)
+        {
+            fretRanges = new double[selectedFrets.Value];
+            CalcFretRanges(1.0);
+            for (int i = 0; i < number; i++)
+            {
+                Neck.ColumnDefinitions.Add(new ColumnDefinition()
+                {
+                    Width = new GridLength(fretRanges[i], GridUnitType.Star)
+                });
+            }
+        }
+
+        private void CreateNeckRows(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                Neck.RowDefinitions.Add(new RowDefinition());
+            }
+        }
+
+        private void CreateNutRows(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                Nut.RowDefinitions.Add(new RowDefinition());
+            }
+        }
+
+        private void CreateFretsOnNeck(int strings, int frets)
+        {
+            for (int i = 0; i < strings; i++)
+            {
+                for (int j = 1; j <= frets; j++)
+                {                    
+                    if (guitarVis.AvailableFrets[i, j])
+                    {
+                        Grid fret = CreateFret(j);
+                        Neck.Children.Add(fret);
+                        Grid.SetColumn(fret, j - 1);
+                        Grid.SetRow(fret, i);
+                    }
+                }
+            }
+        }       
+
+        private void CreateFretsOnNut(int strings)
+        {
+            for (int i = 0; i < strings; i++)
+            {                
+                if (guitarVis.AvailableFrets[i, 0])
+                {
+                    Grid fret = CreateFret(0);
+                    Nut.Children.Add(fret);
+                    Grid.SetRow(fret, i);
+                }
+            }
+        }   
+        
+        private Grid CreateFret(int number)
+        {
+            Ellipse ellipse = new Ellipse()
+            {
+                Style = (Style)TryFindResource("Fret")
+            };
+            TextBlock numberText = new TextBlock()
+            {
+                Text = number.ToString(),
+                Style = (Style)TryFindResource("FretNumber")
+            };
+            Grid fret = new Grid();
+            fret.Children.Add(ellipse);
+            fret.Children.Add(numberText);
+            return fret;
+        }
+
+        private void CalcFretRanges(double value)
+        {
+            if (fret < selectedFrets.Value)
+            {
+                double temp = value / Math.Pow(2, 0.083333);
+                fretRanges[fret] = value - temp;
+                fret++;
+                CalcFretRanges(temp);
+            }
+        }
+
+        private void ClearNeck()
+        {
+            if (Neck != null)
+            {
+                Neck.RowDefinitions.Clear();
+                Neck.Children.Clear();
+                Nut.RowDefinitions.Clear();
+                Nut.Children.Clear();
+            }
         }
 
         private void Strings_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -70,127 +200,6 @@ namespace Scale_Trainer
         {
             selectedKey = (Note.NoteName)((ComboBox)sender).SelectedItem;
             ParameterChanged?.Invoke(this, EventArgs.Empty);
-        }
-
-        private void TryCreateVisualization(object sender, EventArgs e)
-        {
-            try
-            {
-                CreateVisualization();
-            }
-            catch (Exception ex)
-            {
-                Information.Text = ex.Message;
-            }
-        }
-
-        private void CreateNeckColumns(int number)
-        {
-            k = new double[selectedFrets.Value];
-            CalcKoeff(1.0);
-            for (int i = 0; i < number; i++)
-            {
-                Neck.ColumnDefinitions.Add(new ColumnDefinition()
-                {
-                    Width = new GridLength(k[i], GridUnitType.Star)
-                });
-            }
-        }
-
-        private void CreateNeckRows(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                Neck.RowDefinitions.Add(new RowDefinition());
-            }
-        }
-
-        private void CreateFretsOnNeck(int strings, int frets)
-        {
-            for (int i = 0; i < strings; i++)
-            {
-                for (int j = 1; j <= frets; j++)
-                {
-                    Ellipse ellipse = new Ellipse()
-                    {
-                        Width = 10,
-                        Height = 10,
-                        Fill = new SolidColorBrush(Colors.Red),
-                    };
-                    if (guitarVis.AvailableFrets[i, j])
-                    {
-                        Neck.Children.Add(ellipse);
-                        Grid.SetColumn(ellipse, j - 1);
-                        Grid.SetRow(ellipse, i);
-                    }
-                }
-            }
-        }
-
-        private void CreateFretsOnNut(int strings)
-        {
-            for (int i = 0; i < strings; i++)
-            {
-                Ellipse ellipse = new Ellipse()
-                {
-                    Width = 10,
-                    Height = 10,
-                    Fill = new SolidColorBrush(Colors.Red),
-                };
-                if (guitarVis.AvailableFrets[i, 0])
-                {
-                    Nut.Children.Add(ellipse);
-                    Grid.SetRow(ellipse, i);
-                }
-
-            }
-        }
-
-        private void CreateNutRows(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                Nut.RowDefinitions.Add(new RowDefinition());
-            }
-        }
-
-        private void CreateVisualization()
-        {
-            if (selectedStrings.HasValue && selectedFrets.HasValue && selectedTuning != null && selectedScale != null && selectedKey != 0)
-            {
-                guitar = new Guitar(selectedStrings.Value, selectedFrets.Value, selectedTuning);
-                scale = new Scale(selectedScale, selectedKey);
-                guitarVis = new StringedVisualisation(guitar, scale);
-                ClearNeck();
-                CreateNeckRows(selectedStrings.Value);
-                CreateNutRows(selectedStrings.Value);
-                CreateFretsOnNeck(selectedStrings.Value, selectedFrets.Value);
-                CreateFretsOnNut(selectedStrings.Value);
-                Information.Text = "";
-            }
-        }
-
-        void CalcKoeff(double value)
-        {
-            if (fret < selectedFrets.Value)
-            {
-                double temp = value / Math.Pow(2, 0.083333);
-                k[fret] = value - temp;
-                fret++;
-                CalcKoeff(temp);
-            }
-            return;
-        }
-
-        void ClearNeck()
-        {
-            if (Neck != null)
-            {
-                Neck.RowDefinitions.Clear();
-                Neck.Children.Clear();
-                Nut.RowDefinitions.Clear();
-                Nut.Children.Clear();
-            }
         }
     }
 }
